d6d1ebf47e2263a7a4247dc15bafa55986be4a37
Mathias <mathias.grahl@varbi.com>
2025-04-10 17:02:06 +0200

    more config..

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-,
+  server: {
+    host: '::',
+    port: 8080,
+  },
   plugins: [react()],
   resolve: {
     alias: {
