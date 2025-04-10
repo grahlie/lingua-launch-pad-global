4bca57cb3ba903f4969c882799d7baa78b7ffbf1
Mathias <mathias.grahl@varbi.com>
2025-04-10 17:00:56 +0200

    fixed config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+,
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+})
